70ed0956a420d72e60b0fe9841fc5686a53e92b6
valiant.ocampo14 <valiant.ocampo14@gmail.com>
2022-11-13 17:16:31 +0300

    sdfas

--- src/componenets/mainContainer/index.js
+++ src/componenets/mainContainer/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Row, Col} from 'react-bootstrap'
+import {Row, Col, Carousel} from 'react-bootstrap'
 
 
 const MainContent = () => {
@@ -12,7 +12,7 @@ const MainContent = () => {
                     <div className="main-bg">
                     </div>
                     <div className="main-title">
-                        <h1>Annoucement</h1>
+                        <h1>Announcement</h1>
                     </div>
                 </div>
                     <Col sm={4}>
@@ -28,7 +28,48 @@ const MainContent = () => {
                     </Col>
                     <Col>
                         <div className="bg-card">
-                            <div className="main-card"></div>
+                            <div className="main-card" >
+                                <Carousel className="image-content">
+                                    <Carousel.Item interval={30000}>
+                                        <img
+                                        className="d-block w-100"
+                                        src="https://img.freepik.com/free-photo/two-people-holding-hands-together-with-love-warmth-wooden-table_1150-26177.jpg?w=2000"
+                                        alt="First slide"
+                                        height='421px'
+                                        />
+                                        <Carousel.Caption>
+                                        <h5>First slide label</h5>
+                                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                        </Carousel.Caption>
+                                    </Carousel.Item>
+                                    <Carousel.Item interval={30000}>
+                                        <img
+                                        className="d-block w-100"
+                                        src="https://miro.medium.com/max/1400/1*OEnS6-DEn56szCwdOs2mrA.jpeg"
+                                        alt="Second slide"
+                                        height='421px'
+                                        />
+                                        <Carousel.Caption>
+                                        <h5>Second slide label</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        </Carousel.Caption>
+                                    </Carousel.Item>
+                                    <Carousel.Item interval={30000}>
+                                        <img
+                                        className="d-block w-100"
+                                        src="https://cdn.pixabay.com/photo/2018/01/04/19/43/love-3061483__340.jpg"
+                                        alt="Third slide"
+                                        height='421px'
+                                        />
+                                        <Carousel.Caption>
+                                        <h5>Third slide label</h5>
+                                        <p>
+                                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                                        </p>
+                                        </Carousel.Caption>
+                                    </Carousel.Item>
+                                </Carousel>
+                            </div>
                         </div>    
                     </Col>
                 </Row>
--- src/componenets/newContainer/index.js
+++ src/componenets/newContainer/index.js
@@ -4,7 +4,20 @@ const News = () => {
 
     return (
         <div className="footer-News">
-
+                <div className="hmove">
+                    {/* <div className="hitem">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+                    <div className="hitem">Aliquam consequat varius consequat.</div>
+                    <div className="hitem">Fusce dapibus turpis vel nisi malesuada sollicitudin.</div>
+                    <div className="hitem">Pellentesque auctor molestie orci ut blandit.</div> */}
+                    <li className="hitem">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                    <li className="hitem">Aliquam consequat varius consequat.</li>
+                    <li className="hitem">Fusce dapibus turpis vel nisi malesuada sollicitudin.</li>
+                    <li className="hitem">Pellentesque auctor molestie orci ut blandit.</li>
+                    <li className="hitem">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                    <li className="hitem">Aliquam consequat varius consequat.</li>
+                    <li className="hitem">Fusce dapibus turpis vel nisi malesuada sollicitudin.</li>
+                    <li className="hitem">Pellentesque auctor molestie orci ut blandit.</li>
+                </div>
         </div>
     )
 }
