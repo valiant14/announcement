8840bf9037142386b9b9e7cfe803e19338726e61
valiant.ocampo14 <valiant.ocampo14@gmail.com>
2022-11-13 19:27:52 +0300

    asdfasdf

--- src/componenets/Main.js
+++ src/componenets/Main.js
@@ -4,24 +4,50 @@ import bg from '../assets/bg.mp4'
 import SideContainer from './sideContainer'
 import MainContent from './mainContainer'
 import News from './newContainer'
+import axios from "axios";
+
 
 const Main = () => {
     const [currentTime, setCurrentTime] = useState(new Date())
 
+
     useEffect(() => {
-        var timer = setInterval(() => setCurrentTime(new Date()), 1000)
+        var timer = setInterval(() => setCurrentTime(new Date()), 60000)
 
         return function cleanup() {
             clearInterval(timer)
         }
+
+
     },[])
 
-    var time = currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
-    const toRegularTime = (militaryTime) => {
-        const [hours, minutes] = militaryTime.split(':');
-        return `${(hours > 12) ? hours - 12 : hours}:${minutes} ${(hours >= 12) ? 'PM' : 'AM'}`;
+    // var time = currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+    // const toRegularTime = (militaryTime) => {
+    //     const [hours, minutes] = militaryTime.split(':');
+    //     return `${(hours > 12) ? hours - 12 : hours}:${minutes} ${(hours >= 12) ? 'PM' : 'AM'}`;
+    // }
+
+    function GetTime() {
+        var hours = currentTime.getHours()
+        var suffix = '';
+        if (hours > 11) {
+            suffix += "PM";
+        } else {
+            suffix += "AM";
+        }
+        var minutes = currentTime.getMinutes()
+        if (minutes < 10) {
+            minutes = "0" + minutes
+        }
+        if (hours > 12) {
+            hours -= 12;
+        } else if (hours === 0) {
+            hours = 12;
+        }
+        var time = hours + ":" + minutes + " " + suffix;
+        return time;
     }
-    
+
     return (
         <React.Fragment>
             <Row>
@@ -31,7 +57,7 @@ const Main = () => {
                             <SideContainer/>
                         </Col>
                         <Col>
-                            <div className='timeContainer'>{toRegularTime(time)}</div>
+                            <div className='timeContainer'>{GetTime()}</div>
                             <MainContent />
                         </Col>
                     </Row>
--- src/componenets/sideContainer/index.js
+++ src/componenets/sideContainer/index.js
@@ -3,9 +3,43 @@ import Seder from '../../assets/sederLogo.png'
 import {Row, Col} from 'react-bootstrap'
 import Sun from '../../assets/Sun.png'
 import Cloud from '../../assets/Cloud2.png'
-
+import axios from 'axios';
 
 const SideContainer = () => {
+    const [data, setData] = React.useState([]);
+    const [dates, setDates] = React.useState([]);
+
+    React.useEffect(() => {
+        const options = {
+          method: "GET",
+          url: "https://weatherapi-com.p.rapidapi.com/forecast.json",
+          params: { q: "Riyadh", days: "1" },
+          headers: {
+            "X-RapidAPI-Key": "75968c2122mshc78b53ce5e5c7a8p122f51jsnba08182291e0",
+            "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+          }
+        };
+        const value = []
+        const dateValue = []
+        axios
+          .request(options)
+          .then((response) => {
+            console.log(response)
+            response.data.forecast.forecastday.map(item => {
+              value.push(item.day)
+              dateValue.push(item.date)
+              setData(value)
+              setDates(dateValue)
+            })
+           
+          })
+          .catch(function (error) {
+            setData(error);
+          });
+      }, []);
+
+      console.log(dates)
+
     return (
         <React.Fragment>
 
@@ -17,12 +51,16 @@ const SideContainer = () => {
                 </Row>
                 <Row>
                     <Col>
-                        <div className="weather-widget">
-                            <h1 className="Location">Riyadh, 25<span>&#176;</span>C&nbsp;</h1>
+                    {data.map(item => {
+                        return (
+                            <div className="weather-widget">
+                            <h1 className="Location">Riyadh, {item.avgtemp_c}<span>&#176;</span>&nbsp;C</h1>
                                 <img src={Sun} alt="..." className="sun-weather"/>
                                 <img src={Cloud} alt="..." className="cloud-weather"/>
-                            <h3 className="Date">13 November 2022</h3>
-                        </div>
+                            <h3 className="Date">{dates}</h3>
+                            </div>
+                        )
+                    })}
                     </Col>
                 </Row>
             </div>
